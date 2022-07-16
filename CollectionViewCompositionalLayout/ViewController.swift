cb09cb06462b96c739741135f219c56b2bcf82c1
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-16 13:21:55 +0300

    fractionalwidth, '' height set

--- CollectionViewCompositionalLayout/ViewController.swift
+++ CollectionViewCompositionalLayout/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: ViewController.createLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+    }
+    
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        // Item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+        
+        // Group
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalWidth(2/5)
+        ), subitem: item, count: 2)
+        
+        // Sections
+        let section = NSCollectionLayoutSection(group: group)
+        
+        // Return
+        return UICollectionViewCompositionalLayout(section: section)
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
+        
+        return cell
+    }
+}
